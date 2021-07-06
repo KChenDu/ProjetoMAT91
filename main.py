1faae81c5a4e67010ac7d79732738c46502468bc
KChenDu <kelvenchendu@gmail.com>
2021-07-06 10:08:39 -0300

    comments addition and reorganization

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ from air_conditioner import AirConditioner, Mode
 from methods import euler, taylor2, trapezium, mean, rk4, rkf, pc
 import matplotlib.pyplot
 
-Tac = 35
-Tout = 15
-k = 0.03
-kac = 0.1
-Tc_low = 22
-Tc_high = 24
+Tac = 35  # The temperature of the coils
+Tout = 15  # The temperature of the outside air
+k = 0.03  # cooling coefficient(wall)
+kac = 0.1  # cooling coefficient(coils)
+Tc_low = 22  # control temperature(low)
+Tc_high = 24  # control temperature(high)
 
-Tr = 18
-tf = 100
-n = 500
+Tr = 18  # initial room temperature
+tf = 100  # time extension for analysis
+n = 500  # step number
 
 airConditioner = AirConditioner(Tr, Tac, Tout, k, kac, Tc_low, Tc_high, Mode.HEAT)
 
-# t, Trk4 = rk4(airConditioner.act, 0, tf, n, Tr)
-t, T = pc(airConditioner.act, 0, tf, n, Tr)
+t, Teuler = euler(airConditioner.act, 0, tf, n, Tr)
+t, Trk4 = rk4(airConditioner.act, 0, tf, n, Tr)
+t, Tpc = pc(airConditioner.act, 0, tf, n, Tr)
 
-print(t)
-print(T)
 
-matplotlib.pyplot.plot(t, T, label="Test")
+matplotlib.pyplot.plot(t, Teuler, label="Euler")
+#matplotlib.pyplot.plot(t, Trk4, label="Runge-Kutta4")
+#matplotlib.pyplot.plot(t, Tpc, "predictor-corrector")
 matplotlib.pyplot.title('Air conditioning a room')
 matplotlib.pyplot.xlabel('t')
 matplotlib.pyplot.ylabel('room temperature')

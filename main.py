7f14da8f19b87aa414cfd10a1ffb02fdb304e481
KChenDu <kelvenchendu@gmail.com>
2021-07-06 10:35:43 -0300

    refactoration

--- main.py
+++ main.py
@@ -16,15 +16,26 @@ n = 500  # step number
 airConditioner = AirConditioner(Tr, Tac, Tout, k, kac, Tc_low, Tc_high, Mode.HEAT)
 
 t, Teuler = euler(airConditioner.act, 0, tf, n, Tr)
+# t, Ttaylor2 = taylor2(airConditioner.act, airConditioner.act_t, airConditioner.act_y, 0, tf, n, Tr)
+# t, Ttrapezium = trapezium(airConditioner.act, 0, tf, n, Tr)
+# t, Tmean = mean(airConditioner.act, 0, tf, n, Tr)
 t, Trk4 = rk4(airConditioner.act, 0, tf, n, Tr)
+# trkf, Trkf = rkf(airConditioner.act, 0, tf, Tr, 0.1, 0.1, 0.01)
 t, Tpc = pc(airConditioner.act, 0, tf, n, Tr)
 
-
 matplotlib.pyplot.plot(t, Teuler, label="Euler")
-#matplotlib.pyplot.plot(t, Trk4, label="Runge-Kutta4")
-#matplotlib.pyplot.plot(t, Tpc, "predictor-corrector")
+# matplotlib.pyplot.plot(t, Ttaylor2, label="Taylor2")
+# matplotlib.pyplot.plot(t, Ttrapezium, label="Trapezium")
+# matplotlib.pyplot.plot(t, Tmean, label="Mean")
+matplotlib.pyplot.plot(t, Trk4, label="Runge-Kutta4")
+# matplotlib.pyplot.plot(trkf, Trkf, label="Runge-Kutta-Fehlberg")
+matplotlib.pyplot.plot(t, Tpc, label="predictor-corrector")
+
+matplotlib.pyplot.grid()
 matplotlib.pyplot.title('Air conditioning a room')
 matplotlib.pyplot.xlabel('t')
 matplotlib.pyplot.ylabel('room temperature')
 matplotlib.pyplot.legend()
+
+matplotlib.pyplot.savefig('fig.eps')
 matplotlib.pyplot.show()

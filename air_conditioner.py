0fbbaa2f92005eec94a9f1d4cea9688e0f30371a
KChenDu <kelvenchendu@gmail.com>
2021-07-11 18:27:56 -0300

    Methods to find period and action time have been added.

--- air_conditioner.py
+++ air_conditioner.py
@@ -22,6 +22,7 @@ class AirConditioner:
         """
         if k < 0 or kac < 0:
             print("Please check cooling coefficients' coherence.")
+            return
         if Tc_low > Tc_high:
             print("Please check control temperature coherence.")
             return
@@ -29,6 +30,10 @@ class AirConditioner:
         self.Tout = Tout
         self.k = k
         self.kac = kac
+        self.period_clock = -1
+        self.period = -1
+        self.last_start_moment = 0
+        self.action_time = 0
         self.Tc_low = Tc_low
         self.Tc_high = Tc_high
         self.mode = mode
@@ -53,19 +58,33 @@ class AirConditioner:
             if self.state == State.ACTING:
                 if Tr <= self.Tc_low:
                     self.state = State.STOP
+                    self.action_time += t - self.last_start_moment
+                    if self.period == -1:
+                        if self.period_clock == -1:
+                            self.period_clock = t
+                        else:
+                            self.period = t - self.period_clock
                     return self.k * (self.Tout - Tr)
                 return self.k * (self.Tout - Tr) + self.kac * (self.Tac - Tr)
             if Tr > self.Tc_high:
                 self.state = State.ACTING
+                self.last_start_moment = t
                 return self.k * (self.Tout - Tr) + self.kac * (self.Tac - Tr)
             return self.k * (self.Tout - Tr)
         if self.state == State.ACTING:
             if Tr >= self.Tc_high:
                 self.state = State.STOP
+                self.action_time += t - self.last_start_moment
+                if self.period == -1:
+                    if self.period_clock == -1:
+                        self.period_clock = t
+                    else:
+                        self.period = t - self.period_clock
                 return self.k * (self.Tout - Tr)
             return self.k * (self.Tout - Tr) + self.kac * (self.Tac - Tr)
         if Tr < self.Tc_low:
             self.state = State.ACTING
+            self.last_start_moment = t
             return self.k * (self.Tout - Tr) + self.kac * (self.Tac - Tr)
         return self.k * (self.Tout - Tr)
 
@@ -102,3 +121,9 @@ class AirConditioner:
             self.state = State.ACTING
             return -self.k - self.kac
         return -self.k
+
+    def get_period(self):
+        return self.period
+
+    def get_action_time(self):
+        return self.action_time
--- main.py
+++ main.py
@@ -10,24 +10,27 @@ Tc_low = 22  # control temperature(low)
 Tc_high = 24  # control temperature(high)
 
 Tr = 18  # initial room temperature
-tf = 100  # time extension for analysis
-n = 500  # step number
 
 airConditioner = AirConditioner(Tr, Tac, Tout, k, kac, Tc_low, Tc_high, Mode.HEAT)
 
-t, Teuler = euler(airConditioner.act, 0, tf, n, Tr)
+tf = 30  # time extension for analysis
+n = 500  # step number
+
+# t, Teuler = euler(airConditioner.act, 0, tf, n, Tr)
 # t, Ttaylor2 = taylor2(airConditioner.act, airConditioner.act_t, airConditioner.act_y, 0, tf, n, Tr)
 # t, Ttrapezium = trapezium(airConditioner.act, 0, tf, n, Tr)
 # t, Tmean = mean(airConditioner.act, 0, tf, n, Tr)
-t, Trk4 = rk4(airConditioner.act, 0, tf, n, Tr)
+# t, Trk4 = rk4(airConditioner.act, 0, tf, n, Tr)
 # trkf, Trkf = rkf(airConditioner.act, 0, tf, Tr, 0.1, 0.1, 0.01)
 t, Tpc = pc(airConditioner.act, 0, tf, n, Tr)
 
-matplotlib.pyplot.plot(t, Teuler, label="Euler")
+print(f"period = {airConditioner.get_period()}\naction time = {airConditioner.get_action_time()}")
+
+# matplotlib.pyplot.plot(t, Teuler, label="Euler")
 # matplotlib.pyplot.plot(t, Ttaylor2, label="Taylor2")
 # matplotlib.pyplot.plot(t, Ttrapezium, label="Trapezium")
 # matplotlib.pyplot.plot(t, Tmean, label="Mean")
-matplotlib.pyplot.plot(t, Trk4, label="Runge-Kutta4")
+# matplotlib.pyplot.plot(t, Trk4, label="Runge-Kutta4")
 # matplotlib.pyplot.plot(trkf, Trkf, label="Runge-Kutta-Fehlberg")
 matplotlib.pyplot.plot(t, Tpc, label="predictor-corrector")
 
